ead3b68b97c6a2a2a730b9de95752a3a41a6b524
LR <luorui3@lenovo.com>
2019-08-03 17:48:39 +0800

    IncrementAndDecrement

---
+++ src/main/java/com/java/learning/basictype/IncrementAndDecrement.java
@@ -0,0 +1,15 @@
+package com.java.learning.basictype;
+
+public class IncrementAndDecrement {
+
+
+    public static void method1() {
+        int i = 1;
+        i = i++; // 等价于 { temp = i; i = i + 1; return temp;}
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        method1();
+    }
+}
