72886910f29f85ca528363d7a6af1dd03e1bab62
LR <luorui3@lenovo.com>
2019-08-03 17:37:56 +0800

    String function

---
+++ src/main/java/com/java/learning/basictype/Example2.java
@@ -0,0 +1,50 @@
+package com.java.learning.basictype;
+
+public class Example2 {
+
+    /**
+     * 实用的字符串反转
+     * @param str
+     * @return
+     */
+    public static String reverse1(String str) {
+        if ((null == str) || (str.length() <= 1)) {
+            return str;
+        }
+        return new StringBuffer(str).reverse().toString();
+    }
+
+    /**
+     * 递归实现的字符串反转
+     * @param str
+     * @return
+     */
+    public static String reverse(String str) {
+        if ((null == str) || (str.length() <= 1)) {
+            return str;
+        }
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+
+    /**
+     * 检查回文，当然也可以使用StringBuilder进行字符串反转后比较两字符串
+     * @param str
+     * @return
+     */
+    public static boolean isPalindrome(String str) {
+        if (null == str) {
+            return false;
+        }
+        int length = str.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (str.charAt(i) != str.charAt(length - 1 -i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
