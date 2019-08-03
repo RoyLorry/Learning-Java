85d1843850b41766347a2c54931c740de6457640
LR <luorui3@lenovo.com>
2019-08-03 16:15:27 +0800

    基本包装类学习2

--- src/main/java/com/java/learning/basictype/Example1.java
+++ src/main/java/com/java/learning/basictype/Example1.java
@@ -5,7 +5,7 @@ public class Example1 {
     /**
      * 对于基本类型，==判断的是值相等
      * 对于对象，判断的是对象的引用是否一样
-     * 
+     * 因为基本包装类的cache为 -128 ~ 127
      */
     public static void method1(){
         String a = "ssssssssssss";
@@ -17,10 +17,10 @@ public class Example1 {
         Integer e = 1000;
         Integer f = 1000;
         System.out.println(e == f); // false
+        System.out.println(e == 1000); // true 自动拆箱成为int后进行比较
         int g = 1000;
         int h = 1000;
         System.out.println(g == h); // true
-
         System.out.println(3*0.1 == 0.3); // false 浮点数运算会损失精度
     }
 
@@ -37,10 +37,54 @@ public class Example1 {
         // float f = 3.4 浮点数单精度必须写上f，直接写就是double了
         float f = 1.2F;
         double d = 1.2;
+        System.out.println(f == d);
+        byte a = 1;
+        short b = 1;
+        int i = 1;
+        long l = 1L;
+        System.out.println(a == b);
+        System.out.println(b == i);
+        System.out.println(i == l);
+    }
+
+    public static void method4(){
+        double d1 = Double.NaN;
+        double d2 = Double.NaN;
+        double d3 = d1;
+        System.out.println(d1 == d2);
+        System.out.println(d1 == d3);
+    }
+
+    public static void method5() {
+        Integer a = 200;
+        Integer b = 300;
+        Integer c = 500;
+        Integer d = a + b;
+        System.out.println(c == d); // false 比较对象的引用
+        System.out.println(c == (a + b)); // true == 任意一边有运算则进行自动拆箱后进行值比较
+        System.out.println(c.equals(a + b)); // true 比较值
+
+        Long e = 500L;
+        Long f = 300L;
+        System.out.println(e.equals(a + b)); // false 运算后的结果为 int 的 500
+        System.out.println(e.equals(500)); // false
+        System.out.println(e.equals(500L)); // true
+        System.out.println(e.equals(a + f)); // true
+        long g = 500L;
+        int h = 500;
+        System.out.println(g == h); // true
+    }
+
+    public static void method6() {
+        Integer i = 1;
+        i += 1;
+        // 反编译结果展示了自动拆装箱过程，说明自动拆装箱发生在编译过程
+        Integer integer = Integer.valueOf(1);
+        integer = Integer.valueOf(integer.intValue() + 1);
     }
 
     public static void main(String[] args) {
-        
+        method5();
     }
 
 }
