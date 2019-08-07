75691f2439df7df8ef99c4e4f66943e8112c771e
LR <luorui3@lenovo.com>
2019-08-07 22:17:20 +0800

    generics

---
+++ src/main/java/com/java/learning/generics/Example1.java
@@ -0,0 +1,9 @@
+package com.java.learning.generics;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        Object[] objArray = new Long[1];
+        objArray[0] = "aa";
+    }
+}
