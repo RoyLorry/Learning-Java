cc190e443c8499bbab79c37efd331f90419e3f6f
luorui3 <luorui3@lenovo.com>
2019-10-11 17:02:09 +0800

    add javadoc

--- src/main/java/com/java/learning/basicclass/Example3.java
+++ src/main/java/com/java/learning/basicclass/Example3.java
@@ -2,7 +2,12 @@ package com.java.learning.basicclass;
 
 /**
  * 单例类加载初始化
- * 
+ * 类加载的时候在准备过程中为类的静态变量分配内存并初始化默认值 singleton=null，count1=0，count2=0
+ * 类初始化时为类的静态变量赋值和执行静态代码快
+ * singleton 赋值为 new SingleTon() 调用类的构造方法
+ * 调用类的构造方法后 count=1 且 count2=1
+ * 继续为 count1 与 count2 赋值，此时 count1 没有赋值操作
+ * 所以 count1 为 1，但是 count2 执行赋值操作就变为 0
  */
 class Singleton {
     private static Singleton singleton = new Singleton();
